1e29055b2d112efd292b8dc5d6a9ff0ca1c9e1be
Cristian Cortés <cristian.cortes@brm.com.co>
2018-01-25 18:32:53 -0500

    404

---
+++ src/dev/public/404/404.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit} from '@angular/core';
+// import { DOCUMENT } from '@angular/platform-browser';
+// import {WINDOW } from "../../services/window.service";
+
+@Component({
+  templateUrl: './404.component.html'
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+
+}
